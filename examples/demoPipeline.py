4907450bdf5d82398cff5585d72e4fc278090374
Dimitr Imfeld <dimfeld@pm.me>
2025-03-21 13:28:58 +0100

    test convert from paths.json to sim traj

--- examples/demoPipeline.py
+++ examples/demoPipeline.py
@@ -16,19 +16,25 @@ def compute_inverse_kinematics(position, quaternion, last_joint=None):
 
 def generate_trajectory_file(data, filename="trajectory.json"):
     modTraj = []
-    #time_step = 1e6   # 1e6 ns = 1 ms
-    time_step = 1
-    time = 4
+    time_step = 1e6 * 1000   # 1e6 ns = 1 ms
+    #time_step = 3
+    ns_time = 0
+    s_time = 0
     
     for arr in data:
+        ns_time += time_step
+
+        if ns_time >= 1e9:
+            s_time += 1
+            ns_time = 0
+
         positions = [round(float(x), 4) if abs(x) >= 1e-4 else 0.0 for x in arr]
         velocities = [0.0] * 6  # Vélocités à zéro
         modTraj.append({
             "positions": positions,
             "velocities": velocities,
-            "time_from_start": [time, 0]
+            "time_from_start": [int(s_time), int(ns_time)]
         })
-        time += time_step
     
     with open(filename, "w") as f:
         json.dump({"modTraj": modTraj}, f, indent=4)
@@ -57,25 +63,33 @@ def transform_coordinates_to_joint_angles(coordinates, orientations):
 
 # hardcoded, position of the tapped cube near the robot
 # juts for tests
-offset = [84/1000.0, 323/1000.0, 293/1000.0/2.0]
+offset = [250/1000.0, 223/1000.0, 293/1000.0]
 
 paths = {}
 
-path = load_points_cloud()[0][1]
-orientations = [p[3:] for p in path]
-orientations = [[o[0] - 0.001, o[1] - 0.001, o[2] - 0.001, o[3] - 0.001] for o in orientations]
-path = [p[:3] for p in path]
 
-path = [[p[0] + offset[0], p[1] + offset[1], p[2] + offset[2]] for p in path]
+pp = load_points_cloud()
+i = 0
 
-# take only one ine 20 points for faster simulation
-# path = path[::20]
+for ppp in pp:
+    i += 1
+    path = ppp[1]
+    path = [(*p[0], *p[1]) for p in path]
 
-# Compute all joint solutions for each path point
-joint_trajectory = transform_coordinates_to_joint_angles(path, orientations)
-print(f"Computed {len(joint_trajectory)} joint solutions for the trajectory.")
+    orientations = [p[3:] for p in path]
+    path = [p[:3] for p in path]
 
-print(joint_trajectory)
+    path = [[p[0]*1.0 + offset[0], p[1]*1.0 + offset[1], p[2]*1.0 + offset[2]] for p in path]
 
-# Generate trajectory file
-generate_trajectory_file(joint_trajectory, filename="3dmodel_trajectory.json")
+    # Take only every 50th point for faster simulation 
+    # path = path[::50]
+
+    # Compute all joint solutions for each path point
+    joint_trajectory = transform_coordinates_to_joint_angles(path, orientations)
+    print(f"Computed {len(joint_trajectory)} joint solutions for the trajectory.")
+
+    if len(joint_trajectory) != len(path):
+        print(f"Warning: The number of joint solutions ({len(joint_trajectory)}) does not match the number of path points ({len(path)}).")
+
+    # Generate trajectory file
+    generate_trajectory_file(joint_trajectory, filename=f"3dmodel_trajectory__{i}.json")
